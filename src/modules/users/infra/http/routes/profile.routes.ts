2549d739be3551042835f57bba2788e381599ea6
Caio Yoshida Dell G3 <caio.yoshida@yahoo.com.br>
2021-03-23 13:49:15 -0300

    chore: implementing routes validation with celebrate

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
@@ -11,6 +12,28 @@ const profileRouter = Router();
 profileRouter.use(ensureAuthenticated);
 
 profileRouter.get('/', profileController.show);
-profileRouter.put('/', profileController.update);
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      new_password: Joi.string(),
+      new_password_confirmation: Joi.string()
+        .when('old_password', {
+          is: Joi.string().min(1),
+          then: Joi.string().required(),
+        })
+        .concat(
+          Joi.string().when('new_password', {
+            is: Joi.string().min(1),
+            then: Joi.string().required(),
+          }),
+        ),
+    },
+  }),
+  profileController.update,
+);
 
 export default profileRouter;
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -5,6 +5,7 @@ import uploadConfig from '@config/upload';
 
 import EnsureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
+import { celebrate, Segments, Joi } from 'celebrate';
 import UsersController from '../controllers/UsersController';
 import UserAvatarController from '../controllers/UserAvatarController';
 
@@ -14,7 +15,17 @@ const userAvatarController = new UserAvatarController();
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
 
 usersRouter.patch(
   '/avatar',
